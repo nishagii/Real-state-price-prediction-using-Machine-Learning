ad2df59f61c49d8a10330f7a9e9771c0a3ba7190
Nishagi <jeewanthadeherath04@gmail.com>
2025-03-02 22:46:55 +0530

    Refactor code formatting in utility functions for consistency

--- server/util.py
+++ server/util.py
@@ -1,28 +1,30 @@
-import json 
+import json
 import pickle
 
-__locations=None
-__data_columns=None
-__model=None
+__locations = None
+__data_columns = None
+__model = None
+
 
 def get_location_names():
     return __locations
+
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
     global __data_columns
     global __locations
 
-    with open ("./artifacts/columns.json",'r') as f:
-        __data_columns=json.load(f)['data_columns']
-        __locations=__data_columns[3:]
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)["data_columns"]
+        __locations = __data_columns[3:]
 
-    with open ("./artifacts/bangalore_home_price_model.pickle",'rb') as f:
-        __model=pickle.load(f)
+    with open("./artifacts/bangalore_home_price_model.pickle", "rb") as f:
+        __model = pickle.load(f)
 
     print("loading saved artifacts...done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_saved_artifacts()
     print(get_location_names())
